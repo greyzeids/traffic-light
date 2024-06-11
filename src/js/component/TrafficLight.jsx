6092f668e2dbd46866f870894c8a6cde073a6d77
greyzeids <64230328+greyzeids@users.noreply.github.com>
2024-06-11 20:14:43 +0200

    Update - Background added. - Erased rigo baby. - Responsive bootstrap added.

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -14,30 +14,39 @@ const TrafficLight = () => {
     };
 
     return (
-        <main>
-            <div className="p-box"></div>
-            <div className="traffic-light">
-                <div
-                    className={`light red ${
-                        activeLight === "red" ? "active" : ""
-                    }`}
-                    onClick={() => setActiveLight("red")}
-                ></div>
-                <div
-                    className={`light yellow ${
-                        activeLight === "yellow" ? "active" : ""
-                    }`}
-                    onClick={() => setActiveLight("yellow")}
-                ></div>
-                <div
-                    className={`light green ${
-                        activeLight === "green" ? "active" : ""
-                    }`}
-                    onClick={() => setActiveLight("green")}
-                ></div>
-            </div>
-            <div class="d-flex justify-content-center align-items-center vh-100">
-                <button class="btn btn-primary">Alternar</button>
+        <main className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-4">
+                    <div className="p-box"></div>
+                    <div className="traffic-light">
+                        <div
+                            className={`light red ${
+                                activeLight === "red" ? "active" : ""
+                            }`}
+                            onClick={() => setActiveLight("red")}
+                        ></div>
+                        <div
+                            className={`light yellow ${
+                                activeLight === "yellow" ? "active" : ""
+                            }`}
+                            onClick={() => setActiveLight("yellow")}
+                        ></div>
+                        <div
+                            className={`light green ${
+                                activeLight === "green" ? "active" : ""
+                            }`}
+                            onClick={() => setActiveLight("green")}
+                        ></div>
+                    </div>
+                    <div className="text-center mt-3">
+                        <button
+                            className="btn btn-primary"
+                            onClick={toggleLight}
+                        >
+                            Alternar
+                        </button>
+                    </div>
+                </div>
             </div>
         </main>
     );
